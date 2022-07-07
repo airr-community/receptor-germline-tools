26fe0c340a5819c76f8c438d7dd56d62e92b84f7
William Lees <william@lees.org.uk>
2022-07-07 13:44:02 +0100

    First draft

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# receptor_germline_tools
+# Tools for working with AIRR standard germline sets
 
-Tools for working with AIRRC standard germline sets
+AIRR standard germline sets (such as those published on [OGRDB](https://ogrdb.airr-community.org/)) contain rich metadata for each included allele. This metadata may not always be provided in the 
+name. In summary:
+
+- Germline sets may contain sequences with names that have been allocated by IUIS. These are 'IMGT style' names and will match those used by IMGT.
+- Germline sets may also contain sequences that have not yet been allocated names by IUIS. These are provided with 'temporary labels' in a format that
+is designed to be consistent, and readily distinguishable from names allocated by IUIS. They are based around a four-character identifier, for example IGHV-H7DF.
+- Where two or more alleles of the same gene have been identified, the temporary label will include an allele number, for example IGHV-H7DF&ast;01, IGHV-H7DF&ast;02.
+- A subgroup (family) number is never included in the temporary label.
+
+For further details on the naming convention used, and the rationale behind it, please refer to our [poster](https://wordpress.vdjbase.org/index.php/ogrdb_news/germline-set-creation-and-naming/).
+
+We encourage authors of tools and those building analysis pipelines to use the metadata provided in the germline set, rather than parsing the allele name to find the allele number, gene number and so on. This is simpler
+programatically, and also allows for the possibility that some information is not available, for reasons outlined in the poster referenced above.
+To support this, we provide here a command-line tool, add_germline_annotations, which will add commonly-used metadata to a rearrangement file. The tool is intended to be used with AIRR standard rearrangement files, 
+but is sufficientky flexible to work with most rearrangement schemas in CSV or TSV format.
+
+Because some existing tools may have difficulty working with allele names that do not include a subgroup number and/or allele number, we also provide tools that will convert allele names into a format
+that should be acceptable to such tools by inserting dummy values. We hope that these will be less needed over time. The tools can also remove dummy values. We strongly encourage their removal before 
+publication, so that the published names are always consistent and follow the temporary label format.
 
 # Installation
 
--- src/receptor_germline_tools/add_germline_annotations.py
+++ src/receptor_germline_tools/add_germline_annotations.py
@@ -9,6 +9,41 @@
 
 import csv
 import argparse
+import os
+import json
+from collections import namedtuple
+import csv
+
+AlleleData = namedtuple('AlleleData', 'name subgroup allele')
+
+# is the lanem in IgLabel format?
+def is_label(name):
+    if name[4] != '-':       # ie subgroup specified
+        return False
+
+    label = name[5:].split('*')[0]
+
+    if '-' in label or len(label) != 4:
+        return False
+
+    return True
+
+
+# Return dummified name, given the call from the input file
+def dummify(name, germline_data, dummy_subgroup, dummy_allele):
+    if is_label(name):
+        if name in germline_data:
+            dummy_name = name[:4] + germline_data[name].subgroup + '-' + name[5:]
+
+            if '*' not in name:
+                dummy_name += '*' + dummy_allele
+
+            return dummy_name
+        else:
+            dummy_name = name[:5] + dummy_subgroup + name[5:]
+            return dummy_name
+    else:
+        return name
 
 
 def main():
@@ -16,5 +51,61 @@ def main():
     parser.add_argument('input_file', help='alignments to annotate (csv, tsv)')
     parser.add_argument('output_file', help='output with added annotations')
     parser.add_argument('germline_set', help='AIRR standard germline set to use for metadata (JSON)')
+    parser.add_argument('-v', '--v_call', default='v_call')
+    parser.add_argument('-d', '--d_call', default='d_call')
+    parser.add_argument('-j', '--j_call', default='j_call')
+    parser.add_argument('-s', '--dummy_subgroup', default='0')
+    parser.add_argument('-a', '--dummy_allele', default='00')
+
     args = parser.parse_args()
-    
+
+    missing_files = False
+    for filespec in [(args.input_file, 'input_file'), (args.germline_set, 'germline_set')]:
+        if not os.path.isfile(filespec[0]):
+            print(f'{filespec[1]} {filespec[0]} does not exist.')
+            missing_files = True
+
+    if missing_files:
+        exit(1)
+
+    germline_data = {}
+
+    with open(args.germline_set, 'r') as fi:
+        germline_set = json.load(fi)
+
+    for allele_description in germline_set['GermlineSet']['allele_descriptions']:
+        # A blank subgroup designation is used for example in J genes were no subgroup is ever assigned
+        # A null subgroup indicates that a subgroup designation has not been determined
+        subgroup = allele_description['subgroup_designation'] if (allele_description['subgroup_designation'] or allele_description['subgroup_designation'] == "") else args.dummy_subgroup
+        allele = allele_description['allele_designation'] if allele_description['allele_designation'] else args.dummy_allele
+        germline_data[allele_description['label']] = AlleleData(allele_description['label'], subgroup, allele)
+
+    with open(args.input_file, 'r') as fi, open(args.output_file, 'w', newline='') as fo:
+        writer = None
+        tabfile = '\t' in fi.readline()
+        fi.seek(0)
+
+        if tabfile:
+            reader = csv.DictReader(fi, delimiter='\t')
+        else:
+            reader = csv.DictReader(fi)
+
+        for row in reader:
+            if not writer:
+                headers = list(reader.fieldnames)
+                for call in (args.v_call, args.d_call, args.j_call):
+                    if call in headers:
+                        headers.append(call + '_dummy')
+
+                writer = csv.DictWriter(fo, fieldnames=headers)
+                writer.writeheader()
+
+            for call in (args.v_call, args.d_call, args.j_call):
+                if call in row:
+                    names = [dummify(name.replace(' ', ''), germline_data, args.dummy_subgroup, args.dummy_allele) for name in row[call].split(',')]
+                    row[call + '_dummy'] = ','.join([name for name in names])
+                    print(f"{row[call]} -> {row[call + '_dummy']}")
+
+
+if __name__ == "__main__":
+    main()
